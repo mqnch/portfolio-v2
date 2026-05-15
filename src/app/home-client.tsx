7335f2191a3779f31f6f2dee0aa906c61ddfa284
felix <felixpanasks@gmail.com>
2026-05-15 19:22:05 -0400

    style: update cs to computer science

--- src/app/home-client.tsx
+++ src/app/home-client.tsx
@@ -17,7 +17,7 @@ export default function HomeClient({
         <section className="w-full space-y-5 font-mono text-sm text-gray-400">
           <div className="border-t border-gray-900 pt-1 space-y-1"></div>
           <div className="flex items-center gap-2">
-            <p className="text-gray-500 shrink-0 whitespace-nowrap">cs @</p>
+            <p className="text-gray-500 shrink-0 whitespace-nowrap">computer science @</p>
             <div className="flex items-center gap-2">
               <img
                 src="/uwaterloo.svg"
